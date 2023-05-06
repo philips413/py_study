23d0003fdaf23db4b10e045f0a76ff1159807396
Philips413 <philips413@kakao.com>
2023-05-06 16:04:28 +0900

    feature: 파이썬 1일차

---
+++ chapter3/project1/main1-2.py
@@ -0,0 +1,21 @@
+"""
+랜덤 숫자를 추출해서 사용자가 숫자를 맞추는 게임
+"""
+import random
+
+random_number = random.randint(1, 100)
+
+game_count = 1
+
+while True:
+    my_number = int(input("1~100 사이의 숫자를 입력하세요:"))
+
+    if my_number > random_number:
+        print("다운")
+    if my_number < random_number:
+        print("업")
+    elif my_number == random_number:
+        print(f"축하합니다. {game_count}회 만에 맞췄습니다.")
+        break
+
+    game_count = game_count+1
