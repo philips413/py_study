a0e58b08f67bbeb0cb9d33cc881640b56d543ac5
Philips413 <philips413@kakao.com>
2023-05-09 23:44:46 +0900

    feature: 파이썬 4일차

--- chapter3/project3/main3-1.py
+++ chapter3/project3/main3-1.py
@@ -1,6 +1,9 @@
 from gtts import gTTS
+from playsound import playsound
 
 text = "안녕하세요. 파이썬과 40개의 작품들 입니다."
 
 tts = gTTS(text=text, lang='ko')
-tts.save(r"3. 텍스트를 음성으로 변환\hi.mp3")
+tts.save(r"본론.mp3")
+
+playsound("본론.mp3")
