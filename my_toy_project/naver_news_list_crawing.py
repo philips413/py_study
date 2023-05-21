19c33313c9bb8adf5e0cecad1d148be5457e383c
Philips413 <philips413@kakao.com>
2023-05-21 23:14:54 +0900

    feature: 파이썬공부
    1) 크롤링 이후에 데이터를 엑셀에 저장하는 기능개발

--- my_toy_project/naver_news_list_crawing.py
+++ my_toy_project/naver_news_list_crawing.py
@@ -4,6 +4,7 @@
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
 
 naver_news_sidList = [
     {"name": "정치", "sid": 100},
@@ -13,6 +14,11 @@ naver_news_sidList = [
     {"name": "IT/과학", "sid": 105}
 ]
 
+
+write_wb = Workbook()
+sheet = write_wb.active
+sheet.title = "테스트"
+seq = 1
 for newsAgenda in naver_news_sidList :
     response = urlopen(f"https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1={newsAgenda.get('sid')}")
 
@@ -24,4 +30,9 @@ for newsAgenda in naver_news_sidList :
         headline = item.find("a", {"class": "sh_text_headline"})
         aLink = headline["href"]
         headLineTitle = headline.text
+        sheet.append([seq, headLineTitle, aLink])
         print(f"{headLineTitle} : {aLink}")
+        seq = seq + 1
+
+
+write_wb.save("20230521.xlsx")
